cc12bcbdf046e1228d4b847fb3597979cc6d7006
max <max.stienitzka@gmail.com>
2020-01-14 15:07:49 +0100

    Final Commit

---
+++ Assets/MyGame/Script/MoveBall.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveBall : MonoBehaviour {
+
+    //  public GameObject BallPosTopLeft;
+    //  public GameObject BallPosBottomLeft;
+    //  public GameObject BallPosBottomRight;
+    //  public GameObject BallPosTopRight;
+
+    [SerializeField] private GameObject[] BallPositions;
+    [SerializeField] private GameObject[] ball;
+
+    public void MoveTheBall () {
+        Debug.Log (Random.Range (0, 1));
+    }
+}
